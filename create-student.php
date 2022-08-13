063fa4320f4b5f2991395685e731c65e7d9d5d6b
omdasoft <omdasoft@hotmail.com>
2022-08-13 20:54:50 +0200

    create student jquery validation added

--- create-student.php
+++ create-student.php
@@ -33,7 +33,7 @@
                 <div class="container-fluid">
                     <div class="card card-info">
                         <div class="card-body">
-                            <form method="post" action="insert-student.php">
+                            <form method="post" action="insert-student.php" id="regForm">
                                 <fieldset>
                                     <legend><h3>Father Info</h3></legend>
                                     <div class="form-row">
@@ -119,9 +119,103 @@
         <!-- REQUIRED SCRIPTS -->
 
         <?php require_once('includes/partials/scripts.php'); ?>
+        <script src="public/assets/plugins/jquery-validation/jquery.validate.min.js"></script>
+        <script src="public/assets/plugins/jquery-validation/additional-methods.min.js"></script>
         <script>
-            $(function () {
-                $('#example2').DataTable();
+            $(document).ready(function(){
+                $.validator.setDefaults({
+                    submitHandler: function () {
+                        return true;
+                    }
+                });
+                $('#regForm').validate({
+                    rules: {
+                        f_name: {
+                            required: true,
+                            minlength: 3,
+                        },
+                        job: {
+                            required: true,
+                        },
+                        work_location: {
+                            required: true
+                        },
+                        f_address: {
+                            required: true,
+                        },
+                        mobile: {
+                            required: true,
+                            number: true,
+                            minlength: 11
+                        },
+                        stud_name: {
+                            required: true
+                        },
+                        birth_date: {
+                            required: true
+                        },
+                        birth_place: {
+                            required: true
+                        },
+                        stud_address: {
+                            required: true
+                        },
+                        year: {
+                            required: true,
+                            number: true,
+                            maxlength: 4
+                        }
+                    },
+                    messages: {
+                        f_name: {
+                            required: "father name field is required",
+                            maxlength: "father name must be more than 50 characters long"
+                        },
+                        job: {
+                            required: "job field is required",
+                        },
+                        work_location: {
+                            required: "work location field is required"
+                        },
+                        f_address: {
+                            required: "father address is required"
+                        },
+                        mobile: {
+                            required: "mobile field is required",
+                            number: "mobile filed must be number",
+                            minlength: "mobile filed must be at least 11 numbers"
+                        },
+                        stud_name: {
+                            required: "student name field is required"
+                        },
+                        birth_date: {
+                            required: "date birth field is required"
+                        },
+                        birth_place: {
+                            required: "birth place field is required"
+                        },
+                        stud_address: {
+                            required: "student address field is required"
+                        },
+                        year: {
+                            required: "year field is required",
+                            number: "year field must be numbers",
+                            maxlength: "year field must not be more than 4 numbers"
+                        }
+
+                    },
+                    errorElement: 'span',
+                    errorPlacement: function (error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                    },
+                    highlight: function (element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                    },
+                    unhighlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                    }
+                });
             });
         </script>
     </body>
